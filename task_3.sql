41416075c063db3b952e4d28488197987d76bcac
Kurama999905 <117636831+Kurama999905@users.noreply.github.com>
2022-11-30 13:35:38 +0100

    Delete task_3.sql

--- task_3.sql
+++
@@ -1,1 +0,0 @@
-SELECT authors.author_name, Sum(books.sold_copies) as sold_sum FROM authors, books WHERE authors.book_name=books.book_name GROUP BY authors.author_name ORDER BY `sold_sum` DESC LIMIT 3;
