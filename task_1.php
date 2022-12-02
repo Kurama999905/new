535be2956be7846e45bd5df319c823e6a0796e3f
Kurama999905 <117636831+Kurama999905@users.noreply.github.com>
2022-12-02 10:00:54 +0100

    Update task_1.php

--- task_1.php
+++ task_1.php
@@ -1,10 +1,16 @@
 <?php
 $lista_1= array('Ala','Ola','Kot');
-$lista_2= array('Ala','Ada','Kot');
+$lista_2= array('Ala','Ada','Kot', 'ola','oLA');
 function unique_names($lista1, $lista2){
-    $wynik = array_unique(array_merge($lista1,$lista2));
+    $check = array_merge($lista1, $lista2);
+    $wynik = array();
+    foreach ($check as $element_1){
+        if(!in_array(strtolower($element_1), array_map('strtolower', $wynik))){
+            $wynik[]=$element_1;
+        }
+        }
     return $wynik;
-}
+    }
 $names=unique_names($lista_1, $lista_2);
 echo join(', ', $names)
 ?>
