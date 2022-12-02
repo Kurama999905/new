c2fb4ff971e051e6621b87c50f928e1c9d721eab
Kurama999905 <117636831+Kurama999905@users.noreply.github.com>
2022-12-02 14:23:37 +0100

    Update task_1.php

--- task_1.php
+++ task_1.php
@@ -4,16 +4,19 @@ $lista_2 = array('MaRia','olIwia', 'SofIA', 'Ela', 'DORota','Ala', 'Ela', 'Oliwi
 $lista_3 = array('jakub');
 $lista_4 = array('marian');
 $lista_5 = array('MiKoŁaJ');
-function unique_names($lista){
+function unique_names(){
+    $listy = func_get_args();
     $wynik = array();
+    foreach ($listy as $lista){
     foreach ($lista as $element_1){
         if(!in_array(strtolower($element_1), array_map('strtolower', $wynik))){
             $wynik[]=ucfirst(mb_strtolower($element_1));
         }
         }
+    }
     sort($wynik);
     return $wynik;
     }
-$names=unique_names(array_merge($lista_1, $lista_2, $lista_3, $lista_4, $lista_5));
+$names=unique_names($lista_1, $lista_2, $lista_3, $lista_4, $lista_5);
 echo join(', ', $names);
 ?>
